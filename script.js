fdff6d48f9a4834601d646bfbfc4269ac625656f
sundance-lab <nicholasgutteridge512@gmail.com>
2025-06-02 06:42:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -856,7 +856,9 @@ for (let i = 0; i < steps; i++) {
         let sx = Math.floor(texU * textureWidth);
         let sy = Math.floor(texV * textureHeight);
 
-
+const imageData = texCtx.getImageData(sx, sy, 1, 1).data;
+ctx.fillStyle = `rgba(${imageData[0]},${imageData[1]},${imageData[2]},${imageData[3] / 255})`;
+ctx.fillRect(centerX + nx * radius, centerY + ny * radius, 1, 1);
 ctx.restore();
             // Apply shading over the entire planet (both water and land)
             const shadeGradient = ctx.createRadialGradient(

70e9a15a6bd8b690f7783471b518e14155ac13bc
sundance-lab <nicholasgutteridge512@gmail.com>
2025-06-03 01:22:36 -0500

    Update planetRendererWorker.js

--- planetRendererWorker.js
+++ planetRendererWorker.js
@@ -92,7 +92,7 @@ function quat_rotate_vector(q, v) {
 }
 
 function hexToRgb(hex) {
-    if (!hex || typeoff hex !== 'string' || hex.length < 6) return { r:0, g:0, b:0};
+    if (!hex || typeof hex !== 'string' || hex.length < 6) return { r:0, g:0, b:0};
     const r = parseInt(hex.slice(1, 3), 16);
     const g = parseInt(hex.slice(3, 5), 16);
     const b = parseInt(hex.slice(5, 7), 16);

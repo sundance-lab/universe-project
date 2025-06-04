ffaf1a596e3bea5cbc1825221fc9790523011dec
sundance-lab <nicholasgutteridge512@gmail.com>
2025-06-03 22:18:24 -0500

    Create worker_manager.js

---
+++ js/workers/worker_manager.js
@@ -0,0 +1,32 @@
+import * as DOM from '../dom_elements.js';
+import * as State from '../state.js';
+import * as Config from '../config.js';
+// ... any other modules needed for processing worker messages
+
+export let planetVisualWorker = null;
+export let designerWorker = null;
+
+export function setupWorkers() {
+    if (window.Worker) {
+        try {
+            // Instantiate workers and set up onmessage/onerror handlers
+            // These handlers will need to call back into relevant UI/rendering modules
+            // For example, one of the handlers will call a function in rendering.js or ui_elements.js
+            // to put pixels on canvas.
+            planetVisualWorker = new Worker('planetRendererWorker.js');
+            designerWorker = new Worker('planetRendererWorker.js');
+
+            planetVisualWorker.onmessage = (e) => { /* ... process message, update DOM/state ... */ };
+            designerWorker.onmessage = (e) => { /* ... process message, update DOM/state ... */ };
+
+        } catch (err) { /* ... handle error ... */ }
+    } else { /* ... warn about no worker support ... */ }
+}
+
+export function renderPlanetWithWorker(planetData, rotationQuaternion, targetCanvasId, planetRadiusOverride) {
+    // Wrapper function to send data to appropriate worker
+    const workerToUse = (targetCanvasId === 'planet-visual-canvas') ? planetVisualWorker : designerWorker;
+    // ... send message with all necessary data
+}
+// and/or generic postMessage function
+export function postMessageToWorker(workerType, data, transferList) { /* ... */ }

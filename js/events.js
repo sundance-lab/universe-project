c1318c4f04dffcefbf278914e6fa4e6d4da58764
sundance-lab <nicholasgutteridge512@gmail.com>
2025-06-04 01:02:36 -0500

    Update events.js

--- js/events.js
+++ js/events.js
@@ -1,8 +1,8 @@
 // js/ui/events.js
 
-import * as DOM from '../dom_elements.js';
-import * as State from '../state.js';
-import * as Config from '../config.js';
+import * as DOM from './dom_elements.js';
+import * as State from './state.js';
+import * as Config from './config.js';
 import * as MathUtils from '../utils/math_utils.js';
 import * as ScreenManager from './screen_manager.js';
 import * as GalaxyUI from './galaxy_ui.js';

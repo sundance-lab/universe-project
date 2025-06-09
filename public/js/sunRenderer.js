52fe46118c748c35ccfc2de703d501a125086b30
sundance-lab <nicholasgutteridge512@gmail.com>
2025-06-09 02:50:40 -0500

    Update sunRenderer.js

--- public/js/sunRenderer.js
+++ public/js/sunRenderer.js
@@ -202,8 +202,8 @@ update(time) {
     
     try {
         // Slow down the rotation speeds significantly
-        this.sun.rotation.z += 0.002;       // Reduced from 0.00005
-        this.corona.rotation.z -= 0.001;    // Reduced from 0.000025
+        this.sun.rotation.z += 0.2;       // Reduced from 0.00005
+        this.corona.rotation.z -= 0.1;    // Reduced from 0.000025
         
         this.sun.material.uniforms.time.value = time * 0.001;
         if (this.corona.material.uniforms) {

12ae4150085b669081b6ad7229fa7f70688a57a1
sundance-lab <nicholasgutteridge512@gmail.com>
2025-06-11 06:51:48 -0500

    Update galaxyRenderer.js

--- public/js/galaxyRenderer.js
+++ public/js/galaxyRenderer.js
@@ -16,7 +16,7 @@ export const GalaxyRenderer = (() => {
     const GALAXY_THICKNESS = 100;
     const GALAXY_CORE_RADIUS = 200;
     const NUM_ARMS = 5;
-    const ARM_ROTATION = Math.PI;
+    const ARM_ROTATION = 8 * Math.PI;
     const DECORATIVE_STAR_COUNT = 50000;  // Reverted
     const CORE_STAR_COUNT = 20000;       // Reverted
     const DISK_STAR_COUNT = 120000;      // Kept high to fill space
